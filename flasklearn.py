6537df55b300947aec5a800e4eadd399e3421c3a
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2019-09-18 20:21:52 +0545

    Update flasklearn.py

--- flasklearn.py
+++ flasklearn.py
@@ -1,6 +1,10 @@
 #C:\Users\DELL\Desktop>
 from flask import Flask
 app= Flask(__name__)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 @app.route('/')
 def nulljson():
 	return "NO request"
