0bd24281ecaa1461d689ea117a95ea6d3904537f
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2020-01-17 10:27:16 +0545

    Update flasklearn.py

--- flasklearn.py
+++ flasklearn.py
@@ -1,5 +1,6 @@
 #C:\Users\DELL\Desktop>
 from flask import Flask,Response
+import requests
 app= Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -8,6 +9,8 @@ def catch_all(path):
 @app.route('/')
 def nulljson():
 	return "NO request"
-@app.route('/anime/<string:name>',methods=['POST'])
-def findallepisode():
-	pass
+@app.route('/anime/<string:name>')
+def findallepisode(name):
+	headers={'Referer': 'http://vidstreaming.io', 'TE': 'Trailers', 'Alt-Used': 'vidstreaming.io:443', 'X-Requested-With': 'XMLHttpRequest'}
+	r=requests.get('http://vidstreaming.io/ajax-search.html?keyword='+name,headers=headers).json()
+	return r
