a04c92df4714652e6d78b460c0abaffcf0ef0c54
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2020-01-17 10:37:44 +0545

    Create index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+from flask import Flask,Response,request
+import requests
+app= Flask(__name__)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route('/')
+def nulljson():
+	q=request.args.get('q')
+	return q
+@app.route('/asd')
+def findallepisode(name):
+	headers={'Referer': 'http://vidstreaming.io', 'TE': 'Trailers', 'Alt-Used': 'vidstreaming.io:443', 'X-Requested-With': 'XMLHttpRequest'}
+	r=requests.get('http://vidstreaming.io/ajax-search.html?keyword='+name,headers=headers).json()
+	return r
